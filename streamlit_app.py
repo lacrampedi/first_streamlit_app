cfaba76ca0caf0ff282437628fe2cc00183096e6
lacrampedi <47558973+lacrampedi@users.noreply.github.com>
2023-05-13 16:04:36 +0200

    Update streamlit_app.py
    Index of the dataframe set to the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 
 df_my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+df_my_fruit_list = df_my_fruit_list.set_index('Fruit')
 
 streamlit.title("My Mom's New Healthy Diner")
 
