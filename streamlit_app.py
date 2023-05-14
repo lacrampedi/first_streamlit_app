de300a6d68901a25c7df0d22580bf5f48197f85f
lacrampedi <47558973+lacrampedi@users.noreply.github.com>
2023-05-14 17:29:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 df_my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 df_my_fruit_list = df_my_fruit_list.set_index('Fruit')
